5dbb3009a020ab803f1c9cc74a4badd72bf194db
Silvio Andorinha <silvio_andorinha@outlook.com>
2026-06-10 20:10:10 -0300

    feat: entidades de domínio customer e vehicle

---
+++ backend/src/customer-management/domain/value-objects/license-plate.vo.ts
@@ -0,0 +1,18 @@
+export default class LicensePlate {
+  public readonly value: string;
+
+  constructor(value: string) {
+    if (!this.validate(value)) {
+      throw new Error("Invalid license plate format. Must be Mercosul or traditional Brazilian format.");
+    }
+    this.value = value.replace("-", "").toUpperCase();
+  }
+
+  private validate(value: string): boolean {
+    const mercosulRegex = /^[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}$/i;
+
+    const traditionalRegex = /^[A-Z]{3}-?[0-9]{4}$/i;
+
+    return mercosulRegex.test(value) || traditionalRegex.test(value);
+  }
+}
