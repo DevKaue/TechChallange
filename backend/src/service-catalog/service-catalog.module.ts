3745eaf38bb29d9cf5b41f158e79a25530d7e2a4
Thiago Almeida <thiago.almeida@jointecnologia.com.br>
2026-06-17 19:40:17 -0300

    refactor: remover modulo service-catalog (substituido por modulo customer-management)

--- backend/src/service-catalog/service-catalog.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServiceCatalogService } from './service-catalog.service';
-import { ServiceCatalogController } from './service-catalog.controller';
-
-@Module({
-  controllers: [ServiceCatalogController],
-  providers: [ServiceCatalogService],
-})
-export class ServiceCatalogModule {}
